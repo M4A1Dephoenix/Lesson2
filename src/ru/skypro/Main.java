eb59c200cf3cfb91421a91f933d1a6fc4580cc09
M4A1Dephoenix <67238335+M4A1Dephoenix@users.noreply.github.com>
2021-10-15 00:26:19 +0300

    1-6

---
+++ src/ru/skypro/Main.java
@@ -0,0 +1,134 @@
+package ru.skypro;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Задание №1
+        System.out.println("Задание №1");
+        System.out.println("");
+	int clientOS = 1;
+    if (clientOS == 0)
+            System.out.println("Установите версию приложения для iOS по ссылке");
+    else
+            System.out.println("Установите версию приложения для Android по ссылке");
+
+        // Задание №2
+        System.out.println();
+        System.out.println("Задание №2");
+    var clientDeviceYear = 2015;
+    if (clientOS ==0)
+    {
+        if (clientDeviceYear <2015)
+        {
+            System.out.println("«Установите облегченную версию приложения для iOS по ссылке».");
+        }
+        else
+        {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+    }
+    else
+    {
+        if (clientDeviceYear <2015)
+        {
+            System.out.println("«Установите облегченную версию приложения для Android по ссылке».");
+        }
+        else
+        {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+        // Задание №3
+        System.out.println();
+        System.out.println("Задание №3");
+        var year = 2021;
+        if (year % 4 == 0 )
+            System.out.println(year + " Год является високосным");
+        else
+            System.out.println(year + " Год не является високосным");
+
+        // Задание №4
+        System.out.println();
+        System.out.println("Задание №4");
+        var deliveryDistance = 95;
+        var days = 1;
+        if (deliveryDistance <=20)
+        {
+            System.out.println("Потребуется дней "+ days);
+        }
+        else if (deliveryDistance>20 && deliveryDistance<=60)
+        {
+            days += 1;
+            System.out.println("Потребуется дней "+ days);
+        }
+        else if (deliveryDistance>60 && deliveryDistance<=100)
+        {
+            days += 2;
+            System.out.println("Потребуется дней "+ days);
+        }
+        System.out.println();
+        System.out.println("Задание №5");
+        var monthNumber = 12;
+        switch (monthNumber)
+        {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Месяцев только 12");
+
+
+        }
+        // Задание №6
+        System.out.println();
+        System.out.println("Задание №6");
+        var age = 19;
+        int salary = 58_000;
+        if ( age>= 23 ){
+            if (salary >= 50_000 && salary < 80_000) {
+                salary = salary * 3 + salary * 6 / 5;
+            }
+                else if (salary >= 80_000){
+                    salary = salary * 3 + salary * 3 / 2;
+                }
+                else
+                    salary = salary * 3;
+
+
+            }
+        else if (age < 23)
+        {
+            if (salary >= 50_000 && salary < 80_000) {
+                salary = salary * 2 + salary * 6 / 5;
+            }
+            else if (salary >= 80_000){
+                salary= salary * 2 + salary * 3 / 2;
+            }
+            else
+                salary = salary * 2;
+        }
+        System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + salary + " рублей");
+
+        // Задача №7
+        System.out.println();
+        System.out.println("Задача №7");
+
+    }
+}
